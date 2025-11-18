c3d3fab16d94105c91dbd119bc71e8bec636844d
Muskan Kumari <muskan987654kumari@gmail.com>
2025-11-18 21:08:03 +0530

    new file

--- conditional_statements.py
+++ conditional_statements.py
@@ -3,3 +3,24 @@ if age >= 18:
     print("You can vote")
 else:
     print("You can't vote")
+
+username = input("Enter username:")
+password = input("Enter password:")
+if username == "admin" and password == "pass":
+    print("Login Successful")
+elif username != "admin":
+    print("Wrong username")
+else:
+    print("Wrong password")
+
+# Match case
+color = input("Enter the color:")
+match color:
+    case "Green":
+        print("Go")
+    case "Yellow":
+        print("Look")
+    case "Red":
+        print("Stop")
+    case _:
+        print("Wrong colour")
